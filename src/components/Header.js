ffe706ac5f90b5ceca45e4a59219bfc5e0397e87
yousef mohamed <joetiger22222@gmail.com>
2023-04-02 15:51:57 +0200

    homiePage

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,41 +2,20 @@ import React from "react";
 import "../css/Header.css";
 import userImg from "../images/userImg.png";
 import { useState } from "react";
-export default function Header({ onHover, onNotHover }) {
-  const [showDropdown, setShowDropdown] = useState(false);
-
-  const handleMouseEnter = () => {
-    setShowDropdown(true);
-  };
-
-  const handleMouseLeave = () => {
-    setShowDropdown(false);
-  };
+export default function Header() {
+ 
 
   return (
     <div className="headerParent">
       <div className="headerContainer">
         <div className="headerLeft">
-          <h1>test</h1>
+          <h1>Logo</h1>
         </div>
 
         <div className="headerRight">
           <ul className="headerUl">
             <li>MarketPlace</li>
-            {/* <li
-            onMouseLeave={handleMouseLeave}
-            onMouseEnter={handleMouseEnter}>   
-              Courses
-        {showDropdown && ( 
-            <ul>
-              <li>Option 1</li>
-              <li>Option 2</li>
-              <li>Option 3</li>
-            </ul>
-
-        )}
-    
-            </li> */}
+            
             <li class="dropdown">
               <a href="javascript:void(0)" class="dropbtn">
                 Courses
@@ -54,7 +33,16 @@ export default function Header({ onHover, onNotHover }) {
             <button className="headerSignBtn">Login</button>
             <button className="headerSignBtn">Signup</button>
           </div>
-          <img src={userImg} className="userImgHeader" />
+          {/* <img src={userImg} className="userImgHeader" /> */}
+
+          <li class="dropdown">
+              <img src={userImg} class="dropbtn userImgHeader"/>
+              <div class="dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+              </div>
+            </li>
         </div>
       </div>
     </div>
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,35 +4,95 @@ import homePageImg from "../images/homePageImg.png";
 import "../css/HomePage.css";
 import "../css/Header.css";
 import { useState } from "react";
+import badge from "../images/badge.png";
+import quiz from '../images/quiz.png';
+import CourseCard from "./CourseCard";
+import coin from '../images/coin.png'
+import quizCartoon from '../images/quizCartoon.png';
+
 export default function HomePage() {
-  const [showDropdown, setShowDropdown] = useState(false);
-
-  const handleMouseEnter = () => {
-    setShowDropdown(true);
-  };
-
-  const handleMouseLeave = () => {
-    setShowDropdown(false);
-  };
   return (
     <div className="homePageContainer">
-        
       <div className="landingPage">
-        <Header
-          onHover={() => setShowDropdown(true)}
-          onNotHover={() => setShowDropdown(false)}
-        />
-
-        {/* {showDropdown && (
-          <div className="dropdown">
-            <ul>
-              <li>Option 1</li>
-              <li>Option 2</li>
-              <li>Option 3</li>
-            </ul>
-          </div>
-        )} */}
+        <Header />
+        <div className="landingData">
+          <h3>Hello Students, Researchers</h3>
+          <h1>Welcome To Education</h1>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book
+          </p>
+          <button>Join Us Now!</button>
+        </div>
       </div>
+      <div className="badgesDivContainer">
+        <div className="badgeDiv">
+          <img src={badge} />
+          <h1>Best Students</h1>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.
+          </p>
+        </div>
+
+        <div className="badgeDiv">
+          <img src={badge} />
+          <h1>Best Students</h1>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.
+          </p>
+        </div>
+
+        <div className="badgeDiv">
+          <img src={badge} />
+          <h1>Best Students</h1>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.
+          </p>
+        </div>
+      </div>
+
+     <div className="quizContainer">
+        
+        <div>
+
+        <div>
+        <h1>Take A Quiz</h1>
+        <img  src={quizCartoon}/>
+        </div>
+
+        <img className="quizImg" src={quiz}/>
+        </div>
+        
+      </div>
+        
+      <div className="earnPointsContainer">
+        <h1>After Passing The Quiz You Earn Points</h1>
+        <img src={coin}/>
+        <h3>These Points Qualify You To Be A Researcher</h3>
+        
+      </div>
+
+
+
+        <div className="coursesBigDiv">
+        <h3>Couldn't Solve it?, No Problem. Take A Look On Our Courses</h3>
+          <h1>Our Courses</h1>
+      <div className="coursesContainer">
+        
+        <CourseCard/>
+        <CourseCard/>
+        <CourseCard/>
+        <CourseCard/>
+
+      </div>
+      </div>
+
+
     </div>
   );
 }
