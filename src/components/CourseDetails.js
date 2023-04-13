84412b60da885d98abe83dd8978be5a9e67cbe1b
kariem atef <katef4421@gmail.com>
2023-04-13 03:55:24 +0200

    course details

--- src/components/CourseDetails.js
+++ src/components/CourseDetails.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AiOutlineHourglass } from "react-icons/ai";
 import { FaRegNewspaper } from "react-icons/fa";
 import { HiAcademicCap } from "react-icons/hi";
-import "../css/CourseDetails.css"
+import "../css/CourseDetails.css";
+import Header from "./Header.js";
 const CourseDetails = () => {
+  const [videoUrl, setVideoUrl] = useState("");
+
+  useEffect(() => {
+    // Make the GET request to the API endpoint
+    fetch(`https://localhost:7187/api/courses/Videos/1`)
+      .then((response) => {
+        // Check if the response is successful
+        if (!response.ok) {
+          throw new Error("Failed to fetch video.");
+        }
+        // Return the response as a blob
+        return response.blob();
+      })
+      .then((blob) => {
+        // Convert the blob to a URL and set it as the source of the video player
+        const videoUrl = URL.createObjectURL(blob);
+        setVideoUrl(videoUrl);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  console.log(videoUrl);
   return (
+    
     <div className="courseParent">
-      <div className="leftContent">
-        <div className="CourseInfo">
+              <Header />
+
+      <div className="LeftRight">
+        <div className="LeftInfo">
           <h1 className="NameCourse">
             The complete Course python Bootcamp From Zero To Hero
           </h1>
@@ -17,35 +45,32 @@ const CourseDetails = () => {
             natus ducimus similique eaque magni quo beatae, quidem dignissimos
             nobis quisquam maxime!
           </h3>
-          <h4>Created By Admin</h4>
-          <p>Last uodated 3/2/2023</p>
+          <h2>Price</h2>
+          <button>Buy Now</button>
         </div>
-        <div className="Objectives">Objectives</div>
-        <div className="Instructions">Instructions</div>
+        <div className="RightVideo">
+          {videoUrl ? (
+            <video controls src={videoUrl} type="video/mp4" />
+          ) : (
+            <p>Loading video...</p>
+          )}
+          <div className="BottomRightInfo">
+            <p className="headInfo">This Course Include:</p>
+            <p>
+              <AiOutlineHourglass />
+              22 hours
+            </p>
+            <p>
+              <HiAcademicCap />
+              Certificate Of Compiletion
+            </p>
+          </div>
+        </div>
       </div>
 
-      <div className="rightContent">
-        <div className="TopRight">
-          <video src=""></video>
-         
-        </div>
-        <div className="BottomRight">
-        <h2>Price</h2>
-          <button>Buy Now</button>
-          <p>This Course Include:</p>
-          <p>
-            <AiOutlineHourglass />
-            22 hours
-          </p>
-          <p>
-            <HiAcademicCap />
-            Certificate Of Compiletion
-          </p>
-          <p>
-            <FaRegNewspaper />
-            20 Exercise
-          </p>
-        </div>
+      <div className="Bottom">
+        <div className="Objectives">Objectives</div>
+        <div className="Instructions">Instructions</div>
       </div>
     </div>
   );
