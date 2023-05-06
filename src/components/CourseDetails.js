dcbf45553ecee60e4bcd1dc7ce43ae023c41f1a2
kariem atef <katef4421@gmail.com>
2023-05-06 21:26:16 +0300

    Edit Course details design

--- src/components/CourseDetails.js
+++ src/components/CourseDetails.js
@@ -12,7 +12,7 @@ import { useNavigate } from "react-router-dom";
 import ModalForQuiz from "./ModalForQuiz";
 import SectionQuiz from "./SectionQuiz";
 
-
+import video from "../images/001 Course Structure and Projects.mp4";
 const CourseDetails = () => {
   const navigate=useNavigate();
   const [videoUrl, setVideoUrl] = useState("");
@@ -21,7 +21,8 @@ const CourseDetails = () => {
   const [courseSections, setCourseSections] = useState(null);
   const [showVideo, setShowVideo] = useState(false);
   const [showQuiz, setShowQuiz] = useState(false);
-
+  
+const [isQuizes,setIsQuizes] =useState(false)
   const [showAddSection, setShowAddSection] = useState(false);
   const [sectionId,setSectionId]=useState(null);
   const [showUploadVideo,setShowUploadVideo]=useState(false);
@@ -49,101 +50,96 @@ const CourseDetails = () => {
     getCourseSections();
   }, []);
 
-  const SectionCard = ({ section }) => {
+  const SectionCard = ({ section, isQuizes }) => {
     const [activeSection, setAtiveSection] = useState(false);
     const [videosIds, setVideosIds] = useState(null);
     const [sectionQuiz, setSectionQuiz] = useState(null);
-
+  
     function getVideosIds() {
       fetch(`https://localhost:7187/api/Courses/Sections/Videos/${section.id}`)
         .then((res) => res.json())
         .then((data) => setVideosIds(data));
     }
-
-
+  
     function getSectionQuiz() {
       fetch(`https://localhost:7187/api/Quizes/SectionQuiz/${section.id}`)
         .then((res) => res.json())
         .then((data) => setSectionQuiz(data));
     }
-    
-
-
+  
     useEffect(() => {
       getVideosIds();
-      getSectionQuiz()
+      getSectionQuiz();
     }, []);
-    console.log(sectionQuiz)
-    
-   
-    const [video, setVideo] = useState(null);
-    
-
+  
     return (
-      <div className="courseDetailsSectionsContainer">
-      <div className="sectionHeader">
-        <h3
-         onClick={() => {
-          setAtiveSection(!activeSection)
-        }}>
-          {section?.name}
-          {
-            <FaArrowCircleDown
-           
-              style={{
-                transform: activeSection ? "rotate(180deg)" : "none",
-                transition: " 0.2s ease-in-out",
+      <div className="courseDetailsSectionsContainerNew">
+        <div className="sectionHeader">
+          <h3
+            onClick={() => {
+              setAtiveSection(!activeSection);
+            }}
+          >
+            {section?.name}
+            {
+              <FaArrowCircleDown
+                style={{
+                  transform: activeSection ? "rotate(180deg)" : "none",
+                  transition: " 0.2s ease-in-out",
+                }}
+              />
+            }
+          </h3>
+  
+          <div className="sectionIcons">
+            <FaPlusCircle
+              className="plusIcon"
+              onClick={() => {
+                setSectionId(section.id);
+                setShowUploadVideo(true);
               }}
             />
-          }
-        </h3>
-    
-        <div className="sectionIcons">
-          <FaPlusCircle
-            className="plusIcon"
-            onClick={() => {
-              setSectionId(section.id);
-              setShowUploadVideo(true);
-            }}
-          />
-          <FaRegEdit
-            onClick={() => navigate(`/AddQuizToSection/${section.id}`)}
-            className="plusIcon"
-          />
-         
+            <FaRegEdit
+              onClick={() => navigate(`/AddQuizToSection/${section.id}`)}
+              className="plusIcon"
+            />
+          </div>
+        </div>
+  
+        <div
+          className="courseDetailsSectionVideosNew"
+          style={{ display: activeSection ? "flex" : "none" }}
+        >
+          {isQuizes ? (
+            sectionQuiz && (
+              <span
+                className="QuizTitle"
+                onClick={() => navigate(`/SectionQuiz/${section.id}`)}
+              >
+                {section.name} Quiz
+              </span>
+            )
+          ) : (
+            videosIds?.map((video) => (
+              <span
+                onClick={() =>
+                  navigate(`/CourseForStudent/${section.id}/${video.id}`)
+                }
+                className="LinkVideoSection"
+              >
+                <span>{video?.title}</span>
+              </span>
+            ))
+          )}
         </div>
       </div>
-    
-      <div
-        className="courseDetailsSectionVideos"
-        style={{ display: activeSection ? "flex" : "none" }}
-      >
-        {videosIds?.map((video) => (
-          <span
-            // onClick={() => {
-            //   setVideoId(video.id);
-            //   setShowVideo(true);
-            // }}
-            onClick={()=>navigate(`/CourseForStudent/${section.id}/${video.id}`)}
-            className="LinkVideoSection"
-          >
-            <span>{video?.title}</span>
-          </span>
-        ))}
-        {/* <Link
-          onClick={() => setShowQuiz(true)}
-          className="LinkVideoSection"
-        >
-          {section.name} Quiz
-        </Link> */}
-        {sectionQuiz&&<span className="QuizTitle" onClick={()=>navigate(`/SectionQuiz/${section.id}`)}>{section.name} Quiz</span>}
-       
-      </div>
-    </div>
-
     );
   };
   
+  
+
+
+
   const UploadVideoCard = (props) => {
     const [video, setVideo] = useState(null);
     const [videoTitle, setVideoTitle] = useState("");
@@ -400,12 +396,150 @@ const CourseDetails = () => {
   };
 
  
-
+ 
   return (
     <div className="courseParent">
       <Header />
 
-      <div className="LeftRight">
+
+      <div className="AllContentContainer">
+
+        <div className="LeftCourseData">
+          <div className="LeftIntroData">
+            <h1 className="NameCourse">{courseDetails?.name}</h1>
+            <p className="briefCourseNew">{courseDetails?.brief}</p>
+            <h2>Price: 20$</h2>
+            <button className="btnBUY">Buy Now</button>
+          </div>
+      <div className="overFlow">
+          <div className="ContLeftDataInsObj">
+            <div className="ObjectivesNew">
+            <h2>Objectives :</h2>
+              <h3>{courseDetails?.objectives}</h3>
+            </div>
+            <div className="InstructionsNew">
+              <h2>Instructions :</h2>
+              <h3>{courseDetails?.instructions}</h3>
+            </div>
+          </div></div>
+
+        </div>
+
+        <div className="CenterAndRighCourseData">
+            
+            <div className="CourseSectionsData">
+              <div className="courseCOntAddDelSection">
+                <h1 >Course Content</h1>
+                <div className="TrashPlus">
+                <FaPlusCircle
+                  onClick={() => setShowAddSection(true)}
+                  className="addBtn"
+                />
+                <FaTrash onClick={()=>setShowDeleteCourseModal(true)} className="delBtn"/>
+                </div>
+              </div>
+              <div className="ContSectionsNew">
+                {courseSections?.length === 0 && (
+                  <h3 style={{ textAlign: "center" }}>
+                    Click the plus button to start adding sections
+                  </h3>
+                )}
+                {courseSections?.map((section) => {
+                  return <SectionCard section={section} />;
+                })}
+              </div>
+            </div>
+            <div className="contVideoAndQuiz">
+            <div className="RightVideoIntro">
+              <div className="VideoDv">
+                {/* {videoUrl ? (
+                  <video
+                    className="Video"
+                    controls
+                    src={videoUrl}
+                    type="video/mp4"
+                  />
+                ) : (
+                  <h1>Intro Video</h1>
+                )} */}
+                <video  controls>
+                  <source src={video} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              </div>
+
+              <div className="BottomRightData">
+                <h3 className="headInfo">This Course Includes:</h3>
+                <p>
+                  <AiOutlineHourglass />
+                  {courseDetails?.hours}
+                </p>
+                <p>
+                  <HiAcademicCap />
+                  Certificate Of Compiletion
+                </p>
+              </div>
+            </div>
+            <div>
+            <h2 className="h2Quizes">Quizes</h2>
+             <div className="ContSections">
+  
+                {courseSections?.length === 0 && (
+                  <h3 style={{ textAlign: "center" }}>
+                    Click the plus button to start adding sections
+                  </h3>
+                )}
+                {courseSections?.map((section) => {
+  return <SectionCard section={section} isQuizes={isQuizes} />;
+})}
+              </div></div>
+            </div>
+        </div>
+
+
+
+
+        
+        
+
+        
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      {/* <div className="LeftRight">
         <div className="LeftInfo">
           <h1 className="NameCourse">{courseDetails?.name}</h1>
           <p className="briefCourse">{courseDetails?.brief}</p>
@@ -471,7 +605,7 @@ const CourseDetails = () => {
             <h3>{courseDetails?.instructions}</h3>
           </div>
         </div>
-      </div>
+      </div> */}
       {videoId && (
         <VideoCard
           show={showVideo}
